0df6739a02d16765d12363f631ab70e98880d6e8
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2021-11-06 09:38:54 -0400

    Original version with Learn module

---
+++ Model/OrderWithStatus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazingPizza
+{
+    public class OrderWithStatus
+    {
+        public readonly static TimeSpan PreparationDuration = TimeSpan.FromSeconds(10);
+        public readonly static TimeSpan DeliveryDuration = TimeSpan.FromMinutes(1); // Unrealistic, but more interesting to watch
+
+        public Order Order { get; set; }
+
+        public string StatusText { get; set; }
+
+        public bool IsDelivered => StatusText == "Delivered";
+
+        public static OrderWithStatus FromOrder(Order order)
+        {
+            // To simulate a real backend process, we fake status updates based on the amount
+            // of time since the order was placed
+            string statusText;
+            var dispatchTime = order.CreatedTime.Add(PreparationDuration);
+
+            if (DateTime.Now < dispatchTime)
+            {
+                statusText = "Preparing";
+            }
+            else if (DateTime.Now < dispatchTime + DeliveryDuration)
+            {
+                statusText = "Out for delivery";
+            }
+            else
+            {
+                statusText = "Delivered";
+            }
+
+            return new OrderWithStatus
+            {
+                Order = order,
+                StatusText = statusText
+            };
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
