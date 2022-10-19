fc10a7a69fae662792ef4305f72fdbc92fad86ec
Daniel Greco <Daniel.Greco@fdacs.gov>
2022-10-19 16:37:00 -0400

    Initial Commit

---
+++ Data/PizzaStoreContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Data;
+
+public class PizzaStoreContext : DbContext
+{
+    public PizzaStoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<PizzaSpecial> Specials { get; set; }
+}  
