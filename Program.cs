22b2d1b91be0be62c01e0913c6af7199b8feae51
Daniel Greco <Daniel.Greco@fdacs.gov>
2022-10-31 16:36:30 -0400

    Added in the service folder and the Order State Class.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BlazingPizza.Data;
+using BlazingPizza.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
 builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+builder.Services.AddScoped<OrderState>();
 
 var app = builder.Build();
 
