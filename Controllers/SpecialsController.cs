df76028ecf2c5444111a2af0d25dd770a1ec9905
Joshua Hughes <Hughes.J@millfieldschool.com>
2023-06-01 11:44:34 +0100

    test

---
+++ Controllers/SpecialsController.cs
@@ -0,0 +1,24 @@
+﻿using BlazingPizza.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Controllers
+{
+    [Route("specials")]
+    [ApiController]
+    public class SpecialsController
+    {
+        private readonly PizzaStoreContext _db;
+
+        public SpecialsController(PizzaStoreContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+        {
+            return (await _db.Specials.ToListAsync()).OrderByDescending(s => s.BasePrice).ToList();
+        }
+    }
+}
---
+++ Data/PizzaStoreContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Data
+{
+    public class PizzaStoreContext : DbContext
+    {
+        public PizzaStoreContext(
+            DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        public DbSet<PizzaSpecial> Specials { get; set; }
+
+        public DbSet<Topping> Toppings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuring a many-to-many special -> topping relationship that is friendly for serialization
+            modelBuilder.Entity<PizzaTopping>().HasKey(pst => new { pst.PizzaId, pst.ToppingId });
+            modelBuilder.Entity<PizzaTopping>().HasOne<Pizza>().WithMany(ps => ps.Toppings);
+            modelBuilder.Entity<PizzaTopping>().HasOne(pst => pst.Topping).WithMany();
+        }
+
+    }
+}
