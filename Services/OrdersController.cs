87a027431313338215e0298f08a694fa25c98f70
Marie Kauth <marie.kauth@gmail.com>
2023-06-28 23:18:35 -0400

    Implement Order Details

--- Services/OrdersController.cs
+++ Services/OrdersController.cs
@@ -46,4 +46,21 @@ public class OrdersController : Controller
 
         return order.OrderId;
     }
+
+    [HttpGet("{orderId}")]
+    public async Task<ActionResult<OrderWithStatus>> GetOrderWithStatus(int orderId)
+    {
+        var order = await _db.Orders
+            .Where(o => o.OrderId == orderId)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
+            .SingleOrDefaultAsync();
+
+        if (order == null)
+        {
+            return NotFound();
+        }
+
+        return OrderWithStatus.FromOrder(order);
+    }   
 }
